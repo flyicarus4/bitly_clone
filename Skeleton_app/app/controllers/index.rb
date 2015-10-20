77a5eb2ff97640f5f2287d0866813ea798b92d2f
Test <nextacademy@email.com>
2015-10-20 11:04:28 +0800

    starting html

--- Skeleton_app/app/controllers/index.rb
+++ Skeleton_app/app/controllers/index.rb
@@ -5,7 +5,11 @@ end
 
 post '/urls' do
 	@shorten = Url.shorten
-	Url.create(original_url: params[:long_url], short_url: @shorten)
+	Url.create(original_url: params[:long_url], short_url: @shorten, click_count: 0)
 
 	redirect '/'
+end
+
+get '/:short_url' do
+
 end
--- Skeleton_app/app/models/url.rb
+++ Skeleton_app/app/models/url.rb
@@ -2,4 +2,7 @@ class Url < ActiveRecord::Base
 	def self.shorten
 		[*('a'..'z'),*('0'..'9')].shuffle[0,8].join
 	end
+
+	def self.add_count
+	end
 end
